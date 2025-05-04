6796e7885a578df36d8c32a4e298d1bee779442c
nobledeveloper01 <nobleconcepts22@gmail.com>
2025-05-04 02:20:16 +0100

    Completed a full-stack application to stimulate a user management dashboard

---
+++ frontend/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+    return (
+      <div className="text-3xl font-bold underline min-h-screen bg-gradient-to-br from-green-950 to-green-950 flex items-center justify-center px-4 sm:px-6 relative overflow-hidden">
+        Hello world!
+      </div>
+    )
+  }
